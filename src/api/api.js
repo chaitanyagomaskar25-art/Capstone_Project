ae0527ae94e5b294fdc77340d37ec8c863d0d7d6
chaitanyagomaskar25-art <chaitanyagomaskar25@navgurukul.org>
2026-07-24 10:19:57 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/products";
+const BASE_URL = "https://capstone-backend-rorl.onrender.com/products";
 
 export const getProducts = async () => {
   const response = await fetch(`${BASE_URL}`);
